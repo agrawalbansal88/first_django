8b66efdfd711d4fac60799fb5d36cdda8d824e9a
agrawalbansal88 <agrawalbansal88@gmail.com>
2018-09-04 23:46:25 +0530

    Added code to view user input to front page

--- firstDjango/personal/views.py
+++ firstDjango/personal/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import pandas as pd
+from StringIO import StringIO
 
 def index(request):
-    return render(request, 'personal/home.html')
+    if request.method == "POST":
+        paramFile = request.FILES['zerodha_file'].read()
+        data_df = pd.read_csv(StringIO(paramFile))
+        return render(request, 'personal/home.html', {"data_df":data_df.to_html()})
+    else:
+        return render(request, 'personal/home.html')
 
 def contact(request):
     return render(request, 'personal/basic.html', {'content':['I am a good boy', 'am I?']})
