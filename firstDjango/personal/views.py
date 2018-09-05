47fcd8cc7d2643cd89f70312b633591dd734224e
agrawalbansal88 <agrawalbansal88@gmail.com>
2018-09-05 07:40:53 +0530

    Added support uploading tradefile and showing on GUI

--- firstDjango/personal/views.py
+++ firstDjango/personal/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 import pandas as pd
 from StringIO import StringIO
 
+EXPECTED_INPUT_PARAMS = ['client_id', 'trade_date', 'order_execution_time', 'exchange', 'tradingsymbol', 'trade_type', 'quantity', 'price', 'order_id', 'trade_id', 'series']
+
 def index(request):
     if request.method == "POST":
         paramFile = request.FILES['zerodha_file'].read()
         data_df = pd.read_csv(StringIO(paramFile))
-        return render(request, 'personal/home.html', {"data_df":data_df.to_html()})
+        if list(data_df.columns.values) == EXPECTED_INPUT_PARAMS:
+            parse_result = "<h4 style='color:MediumSeaGreen;'>Successfully Uploaded trade file...!!!</h4>"
+            return render(request, 'personal/home.html', {"data_df": data_df.to_html(), 'parse_result': parse_result})
+        else:
+            parse_result = "<h4 style='color:Tomato;'>ERROR while processing trade file...!!!</h4>"
+            return render(request, 'personal/home.html', {'parse_result':parse_result})
     else:
         return render(request, 'personal/home.html')
 
